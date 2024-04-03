e29d67b00d9d98d6cd7c9fbf9e1713a6e5dd5f31
Achhaya Pathak <161013014+achhaypathak@users.noreply.github.com>
2024-04-03 15:57:37 +0530

    Added product

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ app.get("/sum", (req, res) => {
   res.send("The sum is: " + (num + num2));
 });
 
+app.get("/mul", (req, res) => {
+  const num = req.query.num;
+  const num2 = req.query.num2;
+  res.send("The product is: " + (num * num2));
+});
+
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
 });
