83e96a7e93263073de0cc546d9fdbfd7071ad828
Achhaya Pathak <achhaya.pathak@gistimpact.com>
2024-04-11 16:28:15 +0530

    Added Multiplication

--- index.js
+++ index.js
@@ -13,6 +13,15 @@ app.get("/sum", (req, res) => {
   res.send("The sum is: " + sum);
 });
 
+
+app.get("/mul", (req, res) => {
+  const num = req.query.num;
+  const num2 = req.query.num2;
+  const mul = num + num2;
+  res.send("The sum is: " + mul);
+});
+
+
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
 });
