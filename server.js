03c58ec0921ad65c906bddc5d1c790f733902992
Achhaya Pathak <161013014+achhaypathak@users.noreply.github.com>
2024-06-14 15:36:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,11 +18,6 @@ app.get("/sub", (req, res) => {
   res.send("The diff is: " + (num - num2));
 });
 
-app.get("/mul", (req, res) => {
-  const num = req.query.num;
-  const num2 = req.query.num2;
-  res.send("The product is: " + (num * num2));
-});
 
 app.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
